01fabd51bf0a5855a942516d209b6ced3655313e
shainshayusuf <shainshayusuf@gmail.com>
2020-08-30 12:44:22 +0530

    Initial Commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <table>
+      <th>Choose a Date</th>
+      <tbody>
+        <tr>
+          <td>
+    <b-form-datepicker id="datepicker-placeholder" placeholder="Choose a date" v-model="value" local="en"></b-form-datepicker>
+      <p>value:{{value}}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+ data() {
+    return {
+        value: '',
+        
+      }
+    }
+}
+</script>
+
